c37ac94a24995f66ac386809ff9ce9f1f2f153b7
Sub <cclrsahoo@gmail.com>
2021-02-13 08:37:47 -0500

    Bitch

--- image_reader/pixel_reader.py
+++ image_reader/pixel_reader.py
@@ -3,12 +3,12 @@ from openpyxl import Workbook
 from openpyxl.styles import PatternFill, colors
 
 base_width = 225
-im = Image.open('coomer.png', 'r')
+im = Image.open('tommy.png', 'r')
 
 wpercent = (base_width / float(im.size[0]))
 hsize = int((float(im.size[1]) * float(wpercent)))
 img = im.resize((base_width, hsize), Image.ANTIALIAS)
-img.save('coomer_resized.jpg')
+img.save('tommy_resized.jpg')
 
 width, height = img.size
 pix_val = img.getdata()
@@ -30,8 +30,5 @@ for i in range(1, height + 1):
         cell = sheet.cell(column = j, row = i)
         cell.fill = background
 
-# background = PatternFill(fgColor = '00' + rgb_to_hex((122, 104, 119))[1:], fill_type='solid')
-# cell = sheet.cell(column = 1, row = 1)
-# cell.fill = background
 
 workbook.save(filename = 'hello_world.xlsx')
